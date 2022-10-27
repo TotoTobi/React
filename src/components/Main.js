6110cbe9b775776dfbd80d921715810281345e64
TotoTobi <tobiguglin44@gmail.com>
2022-10-27 20:52:01 -0300

    Primera entrega de react

---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Main (props){
+    return(
+    <main>
+        <p>{props.params}</p>
+    </main>
+    );
+};
+
+export default Main
---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function NavBar(){
+    return(
+        <nav className="">                 
+            <a href="">Quienes somos</a>
+            <a href="">Nuestro productos</a>
+            <a href="">Nuestra ubicacíon</a>
+            <span class="material-symbols-outlined">
+                shopping_cart_checkout
+            </span>
+        </nav>
+    );
+};
+
+export default NavBar;
